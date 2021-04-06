2304cab0048d03fa4cde578addd29f7449d8a120
Sonicspeedfly <sonicspeedfly@gmail.com>
2021-04-06 16:05:34 +0500

    v2.4.0

--- pkg/stats/stats_test.go
+++ pkg/stats/stats_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func Test(t *testing.T) {
+func TestCategoryAvg(t *testing.T) {
 	payments := []types.Payment{
 		{Category: "a", Amount: 10_000,},
 		{Category: "a", Amount: 15_000,},
@@ -18,6 +18,27 @@ func Test(t *testing.T) {
 	}
 
 	result := CategoriesAvg(payments)
+
+	if !reflect.DeepEqual(expected, result) {
+		t.Errorf("invalid result, expected: %v, actual: %v", expected, result)
+	}
+}
+
+func TestPeriodsDynamic(t *testing.T) {
+	first := map[types.Category]types.Money{
+		"a" : 10,
+		"b" : 20,
+	}
+	second := map[types.Category]types.Money{
+		"a" : 5,
+		"b" : 3,
+	}
+	expected := map[types.Category]types.Money{
+		"a" : -5,
+		"b" : -17,
+	}
+
+	result := PeriodsDynamic(first,second)
 
 	if !reflect.DeepEqual(expected, result) {
 		t.Errorf("invalid result, expected: %v, actual: %v", expected, result)
